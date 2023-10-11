1d14a7950cde8ab67aab395c815fc6f6f1ec84c1
Dejan Bjelan <dejan.bjelan@telefonica.com>
2023-10-11 14:49:26 +0200

    test disable auth final

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -86,7 +86,7 @@ const Chat = () => {
     }
     
     //const getUserInfoList = async () => {
-    //    const userInfoList = await getUserInfo();
+    //   const userInfoList = await getUserInfo();
         //if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
         //    setShowAuthMessage(true);
        // }
