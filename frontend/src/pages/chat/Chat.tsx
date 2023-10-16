637e836fd0ef9a78ad000d4108df4d315053d0c8
Dejan Bjelan <dejan.bjelan@telefonica.com>
2023-10-16 08:38:40 +0200

    more rebrading

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -535,7 +535,7 @@ const Chat = () => {
                                     aria-hidden="true"
                                 />
                                 <h1 className={styles.chatEmptyStateTitle}>How may I assist you today?</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>CDC chatbot is configured to answer your questions</h2>
+                                <h2 className={styles.chatEmptyStateSubtitle}>CDC AI assistant is configured to answer your questions</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
